5896df07411192013a9e3cb11208c3a10ebf3d64
Vyacheslav <slava.sib@gmail.com>
2021-03-17 18:21:21 +0300

    Update menu-burger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.header__burger').click(function(event){
+    $('.header__burger,.header__link').click(function(event){
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
